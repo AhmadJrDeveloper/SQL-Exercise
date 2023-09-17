aa21ddd0e83d54c94c37ed3cf45e7fda32eaccdb
ahmadjrdeveloper <ahmadsaad200235@gmail.com>
2023-09-17 15:49:02 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -6,3 +6,26 @@ SELECT Points FROM students WHERE Name = "Alex";
 UPDATE students SET Points = Points + 1 WHERE Name = "Basma";
 UPDATE students SET Points = Points - 1 WHERE Name = "Alex";
 #################BASIC QUERIES################################
+
+
+CREATE TABLE graduates (
+ID int NOT NULL PRIMARY KEY ,
+Name text NOT NULL UNIQUE,
+Age int,
+Gender text,
+Points int,
+Graduation date
+);
+
+
+INSERT INTO graduates(ID,Name,Age,Gender,Points)
+SELECT ID,Name,Age,Gender,Points FROM students
+WHERE Name = "Layal";
+
+
+UPDATE graduates SET ID = 1 WHERE Name = "Layal";
+
+UPDATE graduates SET Graduation = 08-09-2018 WHERE Name = "Layal";
+
+
+DELETE FROM students WHERE Name = "Layal";
